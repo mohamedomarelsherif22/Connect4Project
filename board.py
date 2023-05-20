9d783938a9dfe80832fbd72799d59fb185b2655f
belal <belalmohamed2454@gmail.com>
2023-05-20 21:53:23 +0300

    finish_class_Board

--- board.py
+++ board.py
@@ -34,3 +34,78 @@ class Board:
                     print("B", end=" \t")
             print("\n")
         print("-----------------------------------------------------------------------------")
+def _convert_grid_to_color(self, grid):
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == (255, 255, 255):
+                    grid[i][j] = EMPTY
+                elif grid[i][j][0] > 200:
+                    grid[i][j] = RED
+                elif grid[i][j][0] > 50:
+                    grid[i][j] = BLUE
+        return grid
+
+    def _get_grid_cordinates(self):
+        startCord = (47, 42) #51 44
+        cordArr = []
+        for i in range(0, 7):
+            for j in range(0, 6):
+                x = startCord[0] + i * 106 #106
+                y = startCord[1] + j * 105 #104#107
+                cordArr.append((x, y))
+        return cordArr
+
+    def _get_grid_cordinates_for_click(self):
+        startCord = (47, 42) #50, 55
+        cordArr = []
+        for i in range(0, 6):
+            for j in range(0, 7):
+                x = startCord[0] + i * 106   #115
+                y = startCord[1] + j * 105   #112
+                cordArr.append((x, y))
+        return cordArr
+
+    def _transpose_grid(self, grid):
+        return [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))]
+
+    def _capture_image(self):
+        image = ImageGrab.grab()
+        cropedImage = image.crop((LEFT, TOP, RIGHT, BOTTOM))
+        return cropedImage
+
+    def _convert_image_to_grid(self, image):
+        pixels = [[] for i in range(7)]
+        i = 0
+        for index, cord in enumerate(self._get_grid_cordinates()):
+            pixel = image.getpixel(cord)
+            if index % 6 == 0 and index != 0:
+                i += 1
+            pixels[i].append(pixel)
+        return pixels
+
+    def _get_grid(self):
+        cropedImage = self._capture_image()
+        pixels = self._convert_image_to_grid(cropedImage)
+        #cropedImage.show()
+        grid = self._transpose_grid(pixels)
+        return grid
+
+    def _check_if_game_end(self, grid):
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+                if grid[i][j] == EMPTY and self.board[i][j] != EMPTY:
+                    return True
+        return False
+
+    def get_game_grid(self):
+        game_grid = self._get_grid()
+        new_grid = self._convert_grid_to_color(game_grid)
+        is_game_end = self._check_if_game_end(new_grid)
+        self.board = new_grid
+        return (self.board, is_game_end)
+
+    def select_column(self, column):
+        pyautogui.click(
+            self._get_grid_cordinates_for_click()[column][1] + LEFT,
+            self._get_grid_cordinates_for_click()[column][0] + TOP,
+        )
