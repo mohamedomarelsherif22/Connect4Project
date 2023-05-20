e95487383463e66225dc0cdec18204d79734a3f7
belal <belalmohamed2454@gmail.com>
2023-05-20 22:04:20 +0300

    class_board_first

--- board.py
+++ board.py
@@ -109,3 +109,108 @@ def _convert_grid_to_color(self, grid):
             self._get_grid_cordinates_for_click()[column][1] + LEFT,
             self._get_grid_cordinates_for_click()[column][0] + TOP,
         )
+    
+    def copy_board(self, board):
+        return [row[:] for row in board]
+
+    def drop_piece(self, board, row, col, piece):
+        board[row][col] = piece
+
+    def is_valid_location(self, board, col):
+        return board[0][col] == 0
+
+    def get_next_open_row(self, board, col):
+        for r in range(ROW_COUNT):
+            if board[r][col] == 0:
+                return r
+
+    def winning_move(self, board, piece):
+        # Check horizontal
+        for c in range(COLUMN_COUNT - 3):
+            for r in range(ROW_COUNT):
+                if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+                    return True
+
+        # Check vertical
+        for c in range(COLUMN_COUNT):
+            for r in range(ROW_COUNT - 3):
+                if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][c] == piece:
+                    return True
+
+        # Check positively
+        for c in range(COLUMN_COUNT - 3):
+            for r in range(ROW_COUNT - 3):
+                if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and \
+                        board[r + 3][c + 3] == piece:
+                    return True
+
+        # Check negatively
+        for c in range(COLUMN_COUNT - 3):
+            for r in range(3, ROW_COUNT):
+                if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and \
+                        board[r - 3][c + 3] == piece:
+                    return True
+
+    def evaluate_window(self, window, piece):
+        score = 0
+        opp_piece = PLAYER_PIECE
+        if piece == PLAYER_PIECE:
+            opp_piece = AI_PIECE
+        if window.count(piece) == 4:
+            score += 100
+        elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+            score += 5
+        elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+            score += 2
+
+        if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+            score -= 4
+
+        return score
+
+    def score_position(self, board, piece):
+        score = 0
+        # calculate score for center
+        center_count = sum([board[r][COLUMN_COUNT // 2] == piece for r in range(ROW_COUNT)])
+        score += center_count * 3
+
+        # calculate score for horizontal
+        for r in range(ROW_COUNT):
+            row_array = [board[r][c] for c in range(COLUMN_COUNT)]
+            for c in range(COLUMN_COUNT - 3):
+                window = row_array[c:c + WINDOW_LENGTH]
+                score += self.evaluate_window(window, piece)
+
+        # calculate score for Vertical
+        for c in range(COLUMN_COUNT):
+            col_array = [board[r][c] for r in range(ROW_COUNT)]
+            for r in range(ROW_COUNT - 3):
+                window = col_array[r:r + WINDOW_LENGTH]
+                score += self.evaluate_window(window, piece)
+
+        # calculate score for diagonal
+        for r in range(ROW_COUNT - 3):
+            for c in range(COLUMN_COUNT - 3):
+                window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
+                score += self.evaluate_window(window, piece)
+
+        for r in range(ROW_COUNT - 3):
+            for c in range(COLUMN_COUNT - 3):
+                window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
+                score += self.evaluate_window(window, piece)
+
+        return score
+
+    def is_terminal_node(self, board):
+        return self.winning_move(board, PLAYER_PIECE) or self.winning_move(board, AI_PIECE) or len(self.get_valid_locations(board)) == 0
+
+    def get_valid_locations(self, board):
+        valid_locations = []
+        for col in range(COLUMN_COUNT):
+            if self.is_valid_location(board, col):
+                valid_locations.append(col)
+        return valid_locations
+
+
+
+        
