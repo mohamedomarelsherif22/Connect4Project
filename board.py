a4de66511382345f2ee2a743ed1d79d5d64b2562
belal <belalmohamed2454@gmail.com>
2023-05-20 22:07:53 +0300

    class_board_finish

--- board.py
+++ board.py
@@ -210,6 +210,54 @@ def _convert_grid_to_color(self, grid):
             if self.is_valid_location(board, col):
                 valid_locations.append(col)
         return valid_locations
+    
+    def minimax(self, board, depth, alpha, beta, maximizingPlayer):
+        valid_locations = self.get_valid_locations(board)
+        is_terminal = self.is_terminal_node(board)
+        if depth == 0 or is_terminal:
+            if is_terminal:
+                if self.winning_move(board, AI_PIECE):
+                    return (None, 100000000000000)
+                elif self.winning_move(board, PLAYER_PIECE):
+                    return (None, -10000000000000)
+                else:
+                    return (None, 0)
+            else:
+                return (None, self.score_position(board, AI_PIECE))
+        #Max for ai agent
+        if maximizingPlayer:
+            value = -math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = self.get_next_open_row(board, col)
+                b_copy = self.copy_board(board)
+                self.drop_piece(b_copy, row, col, AI_PIECE)
+                new_score = self.minimax(b_copy, depth - 1, alpha, beta, False)[1]
+                if new_score > value:
+                    value = new_score
+                    column = col
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return column, value
+        #Min for computer
+        else:
+            value = math.inf
+            column = random.choice(valid_locations)
+            for col in valid_locations:
+                row = self.get_next_open_row(board, col)
+                b_copy = self.copy_board(board)
+                self.drop_piece(b_copy, row, col, PLAYER_PIECE)
+                new_score = self.minimax(b_copy, depth - 1, alpha, beta, True)[1]
+                if new_score < value:
+                    value = new_score
+                    column = col
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return column, value
+
+
 
 
 
